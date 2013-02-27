4892a7adb42194c83d6547433d3d4f04a8c31d88
Uri Barenholz <uri.barenholz@gmail.com>
2013-02-27 17:26:11 +0200

    updated plot3

--- plot3.py
+++ plot3.py
@@ -19,9 +19,16 @@ fig = figure()
 anums = []
 for i in range(1,3):
     anums.append(ploti(i))
-a1 = subplot(211)
+a1 = subplot(121)
 a1.plot(anums[0][0],anums[0][1],"ro")
-a2 = subplot(212)
+a1.set_xlabel("Growth rate (base 2) [Hour$^{-1}$]")
+a1.set_ylabel("Protein accumulation rate (au)")
+a1.text(0.1,0.9,"(1)",horizontalalignment='right',verticalalignment='bottom',transform=a1.transAxes)
+
+a2 = subplot(122,sharex = a1)
 a2.plot(anums[1][0],anums[1][1],"ro")
+a2.set_xlabel("Growth rate (base 2) [Hour$^{-1}$]")
+a2.set_ylabel("Protein level (au)")
+a2.text(0.1,0.9,"(2)",horizontalalignment='right',verticalalignment='bottom',transform=a2.transAxes)
 
 show()
