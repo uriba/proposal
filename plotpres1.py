0574255e484412606f0c632185ad4bbe760d2950
Uri Barenholz <uri.barenholz@gmail.com>
2013-03-01 10:44:32 +0200

    updated presplot to include two plots

--- plotpres1.py
+++ plotpres1.py
@@ -1,4 +1,4 @@
-from matplotlib.pyplot import plot,show,figure,subplot,ylim
+from matplotlib.pyplot import plot,show,figure,subplot,ylim,title
 from numpy import linspace
 from numpy.random import random,normal
 
@@ -6,15 +6,16 @@ fig = figure()
 a1 = subplot(121)
 a1.set_xlabel("Time [Hour]")
 a1.set_ylabel("Amount")
+title("Logarithmic - non-balanced")
 x = linspace(0,10)
 y = 2**x
 z = abs(normal(0.5,0.2,50)*y)
 a1.plot(x,y,x,z)
 a2 = subplot(122)
-y2= 5*x
-z2 = 2*x
-a2.plot(x,y2,x,z2)
+z2 = 0.5*z
+a2.plot(x,z,x,z2)
 a2.set_xlabel("Time [Hour]")
 a2.set_ylabel("Amount")
+title("Balanced - non-logarithmic")
 
 show()
