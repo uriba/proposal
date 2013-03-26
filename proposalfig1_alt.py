b7ab6a4e51f8c0beea5bce7acd4c52cb22aaf561
Uri Barenholz <uri.b.mail@gmail.com>
2013-03-26 19:22:36 +0200

    updated fig1

--- proposalfig1_alt.py
+++ proposalfig1_alt.py
@@ -7,7 +7,7 @@ axisfontsize = 8
 ticksize = 6
 annotation_size = 8
 file_name_prefix = "graph2"
-initcutoff = 22
+initcutoff = 53 
 activityoffset = 10
 def ploti(i):
     nums = []
@@ -17,10 +17,11 @@ def ploti(i):
             nums.append(map(float,row))
     return nums
 
-def set_ticks(a):
-    for t in a.yaxis.get_major_ticks():
+def set_ticks(p):
+    for t in p.yaxis.get_major_ticks():
         t.label.set_fontsize(ticksize)
-    for t in a.xaxis.get_major_ticks():
+        t.label2.set_fontsize(ticksize)
+    for t in p.xaxis.get_major_ticks():
         t.label.set_fontsize(ticksize)
 
 anums = []
@@ -38,14 +39,35 @@ y1 = np.array(anums[3][1])/1000
 x2 = np.array(anums[3][2])
 y2 = np.array(anums[3][3])/1000
 
-a1 = subplot(322)
-a1.plot(y[initcutoff:],y1[initcutoff:],"g.",y[initcutoff:],y2[initcutoff:],"r.")
-a1.set_ylabel("Protein level\nper OD [au]",fontsize=axisfontsize)
-set_ticks(a1)
-ylim(0,35)
-# xlim(15,25)
+fig.subplots_adjust(right=0.9,wspace=0.35,hspace=0.3)
+a = subplot(311)
+fl1 = np.array(anums[0][3])/1000
+fl2 = np.array(anums[0][5])/1000
+ln1 = a.plot(anums[0][2],fl1,"g",label="YFP")
+ln2 = a.plot(anums[0][4],fl2,"r",label="mCherry")
+a.set_ylabel("Detected Fluorescence [au]",fontsize=axisfontsize)
+a.set_title('E.Coli, M9, 0.2% Glu',fontsize=axisfontsize)
+set_ticks(a)
 
-a2 = subplot(321)
+ax = a.twinx()
+ln3 = ax.plot(anums[0][0],anums[0][1],"b",label="OD")
+ax.set_ylabel("Detected OD",fontsize=axisfontsize)
+a.set_xlabel("Time [Hour]",fontsize=axisfontsize)
+
+lns = ln1+ln2+ln3
+legend(lns,[l.get_label() for l in lns],prop={'size':axisfontsize},loc="upper left")
+set_ticks(ax)
+
+
+a1 = subplot(323)
+a1.plot(y[initcutoff:],y1[initcutoff:],"g.",y[initcutoff:],y2[initcutoff:],"r.")
+a1.set_xlabel("Growth rate [Dbl/Hour]",fontsize=axisfontsize)
+a1.set_ylabel("Protein level per OD [au]",fontsize=axisfontsize)
+set_ticks(a1)
+xlim(0,0.8)
+ylim(0,35)
+
+a2 = subplot(324)
 x3 = np.array(anums[2][0])
 y3 = np.array(anums[2][1])/1000
 x4 = np.array(anums[2][2])
@@ -53,31 +75,27 @@ y4 = np.array(anums[2][3])/1000
 a2.plot(y[initcutoff:],y3[initcutoff-activityoffset:-activityoffset],"g.",
         y[initcutoff:],y4[initcutoff - activityoffset:- activityoffset],"r.")
 a2.set_xlabel("Growth rate [Dbl/Hour]",fontsize=axisfontsize)
-a2.set_ylabel("Protein Acc. rate\nper OD [au]",fontsize=axisfontsize)
+a2.set_ylabel("Protein Acc. rate per OD [au]",fontsize=axisfontsize)
 # legend(('YFP','mCherry'),prop={'size':ticksize})
-# xlim(15,25)
+xlim(0,0.8)
 ylim(0,50)
 set_ticks(a2)
 
-a3 = subplot(324)
+a3 = subplot(325)
 a3.plot(y[initcutoff:],y1[initcutoff:]/y[initcutoff:],"g.",y[initcutoff:],y2[initcutoff:]/y[initcutoff:],"r.")
 a3.set_xlabel("Growth rate [Dbl/Hour]",fontsize=axisfontsize)
-a3.set_ylabel("Protein level\nper OD/Growth rate [au]",fontsize=axisfontsize)
+a3.set_ylabel(r"$ \frac{\mathdefault{Protein\/level\/per\/OD}}{\mathdefault{Growth\/rate}} $ [au]",fontsize=axisfontsize)
+xlim(0,0.8)
+ylim(0,60)
 set_ticks(a3)
 
-a4 = subplot(323)
-a4.plot(y[initcutoff:],y3[initcutoff - activityoffset:-activityoffset]/y[initcutoff:],"g.",
-        y[initcutoff:],y4[initcutoff - activityoffset:-activityoffset]/y[initcutoff:],"r.")
+a4 = subplot(326)
+a4.plot(y[initcutoff:],y3[initcutoff - activityoffset:-activityoffset]/(y[initcutoff:]*y[initcutoff:]),"g.",
+        y[initcutoff:],y4[initcutoff - activityoffset:-activityoffset]/(y[initcutoff:]*y[initcutoff:]),"r.")
 a4.set_xlabel("Growth rate [Dbl/Hour]",fontsize=axisfontsize)
-a4.set_ylabel("Protein Acc. rate\nper OD/Growth rate [au]",fontsize=axisfontsize)
+a4.set_ylabel(r"$ \frac{\mathdefault{Protein\/Acc.\/rate\/per\/OD}}{\mathdefault{Growth\/rate}^2} $ [au]",fontsize=axisfontsize)
+xlim(0,0.8)
+ylim(0,160)
 set_ticks(a4)
 
-a5 = subplot(325)
-a5.plot(y[initcutoff:],y3[initcutoff - activityoffset:-activityoffset]/(y[initcutoff:]*y[initcutoff:]),"g.",
-        y[initcutoff:],y4[initcutoff - activityoffset:-activityoffset]/(y[initcutoff:]*y[initcutoff:]),"r.")
-a5.set_xlabel("Growth rate [Dbl/Hour]",fontsize=axisfontsize)
-a5.set_ylabel("Protein Acc. rate\nper OD/Growth rate^2 [au]",fontsize=axisfontsize)
-set_ticks(a5)
-
-tight_layout()
 fig.savefig('propfig1_alt.pdf')
